c39499edc7ccb83e338b0e0fd41f0a81dd79f8ae
Keith Morton <keefm_6776@yahoo.com>
2019-05-08 22:25:30 +0000

    final commit with improved algorithms to run quicker

--- test_boggle.py
+++ test_boggle.py
@@ -86,7 +86,10 @@ class TestBoggle(unittest.TestCase):
         twoLetterWord = 'AB'
         threeLetterWord = 'ABC'
         notThereWord = 'EEE'
-        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        fullwords = [twoLetterWord, threeLetterWord, notThereWord]
+        stems = ['A', 'AB', 'E', 'EE']
+        dictionary = fullwords, stems
 
         foundWords = boggle.search(grid, dictionary)
 
@@ -101,3 +104,4 @@ class TestBoggle(unittest.TestCase):
         """
         dictionary = boggle.get_dictionary('words.txt')
         self.assertGreater(len(dictionary), 0)
+    
